dabc413dfc642310a55b7471be69088f167604ec
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-07 21:37:29 -0500

    add new function: uppcase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     str1 = ''
-    for i in range(0,len(str)):
+    for i in range(len(str)):
         if str[i] >= 'a' and str[i] <= 'z':
             str1 = str1 + chr(ord(str[i]) - 32)
         else:
