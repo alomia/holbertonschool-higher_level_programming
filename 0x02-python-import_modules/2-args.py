1ba275804834550fd858e7b360f756517e4a087d
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-08 20:47:56 -0500

    add new program: script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    strCount = len(argv) - 1
-    if strCount == 0:
-        print('{} {}'.format(strCount, "arguments."))
-    elif strCount == 1:
-        print('{} {}'.format(strCount, "argument:"))
-    elif strCount > 2:
-        print('{} {}'.format(strCount, "argument:"))
-    else:
-        print('{} {}'.format(strCount, "arguments:"))
-        for i in range(1, strCount):
-            print('{}: {}'.format(i, argv[i]))
+
+    strCount = (len(argv)) - 1
+
+    print('{} argument{}{}'
+        .format(strCount, "" if strCount == 1 else "s", ":" if strCount != 0 else "."))
+
+    for i in range(1, strCount + 1):
+        print('{}: {}'.format(i, argv[i]))
