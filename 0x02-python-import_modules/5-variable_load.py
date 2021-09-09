aaefefd8945eab66d69dce218c369212796db126
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-08 22:42:17 -0500

    git add new program: imported variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__name__":
-    import variable_load_5
-    a = variable_load_5.a
-    print('{}'.format(a))
+    from variable_load_5 import a
+    print('{:}'.format(a))
