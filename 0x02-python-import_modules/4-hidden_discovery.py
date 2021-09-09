3d32eaeb98d8cb214e8984ee55621321b34319ca
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-08 22:33:22 -0500

    add new program: print all the name

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print('{:s}'.format(i))
+    a = dir(hidden_4)
+    for i in range(len(a)):
+        if a[i][:2] != "__":
+            print(a[i])
