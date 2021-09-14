91ec234c704dd6b9e11bb9c6ec243b02afe54051
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-13 21:14:55 -0500

    update file: Can you C me now

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    str1 = ""
+    if my_string is None:
+        return None
+    else:
+        for i in my_string:
+            if i == 'c' or i == 'C':
+                continue
+            str1 += i
+        return str1
