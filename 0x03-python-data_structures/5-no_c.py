ef68fe281a40013cd9239d142470f33b5ed2a56e
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-13 21:26:02 -0500

    update file: Can you C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     str1 = ""
-    if my_string is None:
-        return None
-    else:
-        for i in my_string:
-            if i == 'c' or i == 'C':
-                continue
-            str1 += i
-        return str1
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        str1 += i
+    return str1
+        
