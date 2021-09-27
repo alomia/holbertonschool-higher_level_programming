51f5a5fb4a385d95c2c6e133fcdd6b2033c2c0b1
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-09-27 17:46:07 -0500

    add function: prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except ValueError:
-        print("{} is not an integer".format(value))
+        print(value, "is not an integer")
     return value
