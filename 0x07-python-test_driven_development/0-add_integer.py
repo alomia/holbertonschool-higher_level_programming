7666855fad33179d55afe07b23fcf058375611a4
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-07 17:30:27 -0500

    add file: function add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines the add integer function"""
+
+
+def add_integer(a, b=98):
+    """
+    Args:
+        a: first number
+        b: second number
+    Return:
+        int: the sum a + b
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer") 
+    
+    return int(a) + int(b)
