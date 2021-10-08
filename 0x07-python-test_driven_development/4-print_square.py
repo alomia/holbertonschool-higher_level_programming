5952bbc0d76b40518aa5e9255da7e1fd9d090b1b
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-07 23:17:05 -0500

    add function square and doc file test

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,14 +7,13 @@ def print_square(size):
     Args:
         size: size of the square
     """
-
+    if type(size) != int:
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
-    if type(size) is not int:
+    if type(size) == float and size < 0:
         raise TypeError("size must be an integer")
     for i in range(size):
         for j in range(size):
-            print("#", end='')
+            print("#", end="")
         print("")
