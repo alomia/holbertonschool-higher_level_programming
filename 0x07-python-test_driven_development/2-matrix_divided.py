e83bbe373abf5fc75c63d5b3db2bffe68bbb7ccd
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-07 21:17:55 -0500

    add file .py  and file .txt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a function that divides all elements of a matrix"""
 
+
 def matrix_divided(matrix, div):
     """
     Arg:
@@ -9,7 +10,6 @@ def matrix_divided(matrix, div):
     Return:
         list: new matrix
     """
-
     for row in matrix:
         if type(row) != list:
             raise TypeError("matrix must be a matrix (list of lists) of \
@@ -40,4 +40,3 @@ integers/floats")
         if len(row) != length:
             raise TypeError("Each row of the matrix must have the same size")
     return [[round(i/div, 2) for i in row] for row in (matrix)]
-
