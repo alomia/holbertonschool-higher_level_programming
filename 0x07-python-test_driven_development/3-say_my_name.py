52d5cb6cffd324686c8894f3f5541bc77f1a6c5e
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-07 22:22:28 -0500

    add function and doc test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,8 +8,8 @@ def say_my_name(first_name, last_name=""):
         first_name: saves the first name
         last_name: saves the last name
     """
-    if type(first_name) != str:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
