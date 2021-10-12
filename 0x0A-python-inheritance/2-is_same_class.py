6ad93aee52b55ab2b08f57b5b2a1606f62f022ef
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-11 23:43:11 -0500

    add file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""returns True if the object is exactly an
+instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks if onj instance of class
+    """
+    return True if type(obj) is a_class else False
