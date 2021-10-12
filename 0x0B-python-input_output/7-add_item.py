ed36c98da3f75cb10ebe8fd5bea6673ad5586776
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-12 18:26:07 -0500

    add file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 """
 from sys import argv
 
-save_to_file = __import__('5-save_to_json_file').save_to_file
-load_from_file = __import__('6-load_from_json_file').load_from_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_file
 
 filename = 'add_item.json'
 
