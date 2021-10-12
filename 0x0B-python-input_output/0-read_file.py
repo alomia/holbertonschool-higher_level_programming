ddd7e772e11c48438a08a3416f41d1ffa8e11672
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-12 12:01:37 -0500

    add file: function that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+that reads a text file (UTF8) and prints it to stdout    
+"""
+
+
+def read_file(filename=""):
+    """
+    Args:
+        filename: name of file
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
