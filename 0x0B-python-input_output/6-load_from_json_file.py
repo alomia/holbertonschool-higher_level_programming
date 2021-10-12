bf873c3892572061fedcb4d9dc70fb12706ef687
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-12 17:23:28 -0500

    add file: 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    creates an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Args:
+        filename: name of file
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
