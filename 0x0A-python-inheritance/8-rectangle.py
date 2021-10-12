c7005f34ea77ab041cd7080a625a1c85bb5cb7c6
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-11 23:58:12 -0500

    add file

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write an class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Create class
+    """
+
+    def __init__(self, width, height):
+        """subclass
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
