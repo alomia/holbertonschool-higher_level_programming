26fd12d89d112099599894bfa23ca882a2e28024
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-12 18:46:57 -0500

    add file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """
 Script that append arguments and create a new file
 """
+from sys import argv
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 try:
     my_list = load_from_json_file(filename)
 except:
     my_list = []
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, filename)
+
+save_to_json_file(my_list + argv[1:], filename)
