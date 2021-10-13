b2afeff7709ec9c85cb6dba52b496a78efe5a88d
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-12 19:50:35 -0500

    add file: 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class Student that defines a student by
+"""
+
+
+class Student:
+    """Public instance attributes"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
