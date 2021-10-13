a18a3c84cbfcf31ae1f5c59ad52dcf72dd321ecd
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-13 17:46:33 -0500

    add file 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,12 +6,12 @@ Return a Pascal's Triangle
 
 def append_after(filename="", search_string="", new_string=""):
     """Return a triangle"""
-	acumulado = ""
+	txt = ""
 	with open(filename, "r") as f:
 		for line in f:
-			acumulado += line
+			txt += line
 			if search_string in line:
-          acumulado += new_string
+          txt += new_string
 		
 	with open(filename, "w") as w:
-		w.write(acumulado)
+		w.write(txt)
