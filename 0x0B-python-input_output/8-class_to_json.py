e12f7b4bae1f8be72a0b64857f1b543e2fb5ba0a
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-12 20:03:00 -0500

    add file 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    returns the dictionary description with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """
+    Args:
+        obj: save value
+    """
+    return obj
