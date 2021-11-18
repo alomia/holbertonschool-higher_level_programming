fe36379e069a9444eb017bc10d8a8b45171a1831
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-11-17 23:36:52 -0500

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- States table
 CREATE DATABASE hbtn_0d_usa;
 CREATE TABLE hbtn_0d_usa.states(
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(256) NO NULL
+        'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        'name' VARCHAR(256) NO NULL
 );
