096ee449ff8153bba26baafbbf9581cca204f97d
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-14 16:48:54 -0500

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const number = process.argv[2];
 if ((process.argv[2] == null) || (number >= 'a' && number <= 'z')) {
   console.log('Not a number');
 } else {
-  console.log('My number:', parseInt(number));
+  console.log('My number: ' + parseInt(number));
 }
