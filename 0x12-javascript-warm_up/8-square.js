8f8425437cbee1000ed97bcc14e8effa979cabd9
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-14 17:59:50 -0500

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const argv = parseInt(process.argv[2]);
+
+if (isNaN(argv)) {
+        console.log('Missing size');
+} else {
+  for (let height = 1; height <= argv; height++) {
+    let character = '';
+    for (let width = 1; width <= argv; width++) {
+      character += 'X'
+    }
+    console.log(character);
+  }
+}
