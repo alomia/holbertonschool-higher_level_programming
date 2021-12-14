31362000a8fc746e6a6dedee5ab2757e66ece165
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-13 23:52:38 -0500

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let step = 0; step <= languages.length - 1; step++) {
+  console.log(languages[step]);
+}
