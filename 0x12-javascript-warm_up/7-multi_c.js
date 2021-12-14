f3de3be8fa62c92b25a6ff5801304bcfc5a85c97
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-14 17:27:00 -0500

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = parseInt(process.argv[2]);
+
+if (isNaN(argv)) {
+        console.log('Missing number of occurrences');
+} else {
+  for (let step = 1; step <= argv; step++) {
+    console.log('C is fun');
+  }        
+}
