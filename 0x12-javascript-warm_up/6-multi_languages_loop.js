cba5b928debf88da4baa044d1145126af592afed
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-14 17:12:51 -0500

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const arrayString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let step = 0; step <= arrayString.length - 1; step++) {
+  console.log(arrayString[step]);
+}
