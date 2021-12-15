d01d1f9124e7b2b722321d09141b5351d928377a
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-14 19:29:57 -0500

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
-const argc = process.argv.length - 2;
+const args = process.argv;
 
-arrayNumbers = [];
+myArray = [];
 
-if (argc <= 1) {
-  console.log('0');
-} else {
-  for (let step = 1; step <= argc; step++) {
-    arrayNumbers.push(process.argv[step + 1]);
-  }
-
-  arrayNumbers.sort();
-
-  console.log(parseInt(arrayNumbers[argc - 1]));
+if (args.length <= 3) {
+	console.log(0)
+} else if (args.length > 3){
+	args.forEach(function (element) {
+		const x = parseInt(element);
+		if (isNaN(x)) {
+			myArray.push('');
+		}
+		myArray.push(element);
+	});
+	console.log(myArray);
 }
