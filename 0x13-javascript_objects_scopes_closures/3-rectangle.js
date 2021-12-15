ec3cf2c798f9871116887dcdcb70bc0937fff7a6
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-15 15:35:37 -0500

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ module.exports = class Rectangle {
     }
   }
 
-   print () {
+  print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write('X');
