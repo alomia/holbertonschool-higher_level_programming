5d36bd8f14fae4a3734df5fe1e880ce64b7e868d
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-24 13:51:59 -0500

    Filter states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 
-import Py
 import MySQLdb
 from sys import argv
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC;")
-        query = cur.fetchall()
 
-        for i in query:
-                print(i)
+def connection():
+    """
+    Simple Query Function
+    """
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to DB")
+        return 0
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
-        cur.close()
-        consult.close()
+
+connection()
 
