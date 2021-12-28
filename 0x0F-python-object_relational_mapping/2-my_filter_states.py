84af403d3db8deed31891eaee11653a195fea92c
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-28 04:26:06 -0500

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N)
+from the hbtn_0e_0_usa database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+
+    cur = conn.cursor()
+    cur.execute(
+            f"SELECT *\
+            FROM states\
+            WHERE states.name = '{argv[4]}'\
+            ORDER BY states.id ASC"
+        )
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
