607903efe7c2d442d4ed6148b62f87bdd6924f69
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-12-31 19:26:05 -0500

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,12 +6,11 @@ State and an instance Base = declarative_base():
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 
-class States:
-        """class that ninherits from Base"""
-        __tablename__ = 'states'
-        id = Column(Integer, Primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
+class State(Base):
+    """class that inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
