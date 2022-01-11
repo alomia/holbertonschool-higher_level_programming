aa691358394024a4222db4c8c88359fbe2ebf339
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-01-11 00:08:47 -0500

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-What's my status? #0
-"""
+"""What's my status? #0"""
+
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as res:
-        html = res.read()
-        print('Body response:')
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response::")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
