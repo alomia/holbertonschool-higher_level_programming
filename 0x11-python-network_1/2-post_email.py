44c70a33cd0309b62a5b4f4c13c2517e0ccf2dff
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-01-12 22:13:24 -0500

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""
-POST an email #0
-"""
-if __name__ == "__main__":
-    import urllib.parse as parse
-    import urllib.request as request
-    import sys
+"""POST an email #0"""
 
-    email = parse.urlencode(sys.argv[2]).encode('utf-8')
-    with request.urlopen(sys.argv[1], email=email) as res:
-        x = res.read()
-    print(x)
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values).encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        answer = response.read().decode('utf-8')
+        print(answer)
+
