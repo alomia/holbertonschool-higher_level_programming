15dbf66b179122971d3866c1755147866e750423
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-01 18:54:07 -0500

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
-fs.writeFile(file, text, function (err, data) {
+fs.writeFile(file, text, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
