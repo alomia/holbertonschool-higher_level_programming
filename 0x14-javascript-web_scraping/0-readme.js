f50aeec412affd963a4c93ba826b99dfc333cafe
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-01 18:34:10 -0500

    Reads the contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const file = process.argv[2]
 const fs = require('fs');
+const file = process.argv[2];
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
