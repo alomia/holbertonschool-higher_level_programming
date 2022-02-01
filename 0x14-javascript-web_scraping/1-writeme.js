d64979f936c5a3417bbae783342ba83f026a3d92
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-01 18:44:52 -0500

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const text = process.argv[3];
+fs.writeFile(file, text, function(err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data)
+});
