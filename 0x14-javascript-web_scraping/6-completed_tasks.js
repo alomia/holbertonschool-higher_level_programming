300115266fc8b33f1f89bc370ed0bfc88c538053
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-01 22:55:29 -0500

    How many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+async function taskCompleted () {
+  await request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    }
+    const obj = JSON.parse(body);
+    const data = obj;
+    const dict = {};
+
+    for (const user of data) {
+      if (user.completed) {
+        if (dict[user.userId] === undefined) {
+          dict[user.userId] = 1;
+        } else {
+          dict[user.userId] += 1;
+        }
+      }
+    }
+    console.log(dict);
+  });
+}
+
+taskCompleted();
