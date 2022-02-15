e1dcf4c47f994aae1c085b9ef28f173aa6ce84a6
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-15 03:46:58 -0500

    Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-    $.ajax({
-        url: 'https://swapi-api.hbtn.io/api/films/?format=json',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data){
-            for (let item of data.results) {
-                $("#list_movies").append(`<li>${item.title}</li>`);
-            }
-        },
-    });
+$(document).ready(function () {
+  $.ajax({
+    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      for (const item of data.results) {
+        $('#list_movies').append(`<li>${item.title}</li>`);
+      }
+    }
+  });
 });
