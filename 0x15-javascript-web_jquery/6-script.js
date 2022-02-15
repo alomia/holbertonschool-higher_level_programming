864c8da456289aaf9c6090f28aa72aab4ae187fe
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-15 03:40:23 -0500

    Change the text

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $("#update_header").click(function(){
-        $("header").empty();
-        $("header").append("New Header!!!");
-    });
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').empty();
+    $('header').append('New Header!!!');
+  });
 });
